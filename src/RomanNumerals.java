395d624f551305f1418437b7c4401fdffbc7b20e
chouaibHamek <chouaib.hamek@student.oulu.fi>
2017-10-12 14:22:18 +0300

    Implement Roman Numeral conversion - TDD

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,67 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+	
+	private int convertDigit(char c) {
+		switch(c) {
+			case 'I': return 1;
+			case 'V': return 5;
+			case 'X': return 10;
+			case 'L': return 50;
+			case 'C': return 100;
+			case 'D': return 500;
+			case 'M': return 1000;
+			default: return -1;
+		}
+	}
+	
+	public boolean checkValidity(String romanNum) {
+		// empty strings are not valid, we check for the input validity using this nice pattern :)
+		return (romanNum.length()!=0 && romanNum.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"));
+	}
+	
+	public int convertToInteger(String romanNum) throws InvalidInputException {
+		int arabicNum = 0;
+		if (!checkValidity(romanNum)) {
+			throw new InvalidInputException();
+		} else {
+			// the input is valid and not empty - no zero in Roman numbers
+			int i=0;
+			while (i<romanNum.length()) {
+				if (i+1 < romanNum.length()) {
+					if (romanNum.charAt(i) == 'I' && romanNum.charAt(i+1) == 'V') {
+						arabicNum+= 4;
+						i+=2;
+					}
+					else if (romanNum.charAt(i) == 'I' && romanNum.charAt(i+1) == 'X') {
+						arabicNum+= 9;
+						i+=2;
+					}
+					else if (romanNum.charAt(i) == 'X' && romanNum.charAt(i+1) == 'L') {
+						arabicNum+=40;
+						i+=2;
+					}
+					else if (romanNum.charAt(i) == 'X' && romanNum.charAt(i+1) == 'C') {
+						arabicNum+= 90;
+						i+=2;
+					}
+					else if (romanNum.charAt(i) == 'C' && romanNum.charAt(i+1) == 'D') {
+						arabicNum+=400;	
+						i+=2;
+					}
+					else if (romanNum.charAt(i) == 'C' && romanNum.charAt(i+1) == 'M') {
+						arabicNum+= 900;
+						i+=2;
+					}
+					else {
+						arabicNum+= convertDigit(romanNum.charAt(i));
+						i++;
+					}
+				} else {
+					arabicNum+= convertDigit(romanNum.charAt(i));
+					i++;
+				}
+			}
+		}
+		return arabicNum;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,202 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	
+	// Tests for the input validity feature.
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInvalid_empty() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity(""));
 	}
-
+	
+	@Test
+	public void testInvalid_numeric() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("01112145"));
+	}
+	
+	@Test
+	public void testInvalid_letter() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("arabic letters are fun"));
+	}
+	
+	@Test
+	public void testInvalid_specialLetter() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("##$@!#$!@#"));
+	}
+	
+	@Test
+	public void testInvalid_rule1_1() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("IIIIC"));
+	}
+	
+	@Test
+	public void testInvalid_rule1_2() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("XXXXC"));
+	}
+	
+	@Test
+	public void testInvalid_rule1_3() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("CCCCD"));
+	}
+	
+	@Test
+	public void testInvalid_rule2_1() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("IVV"));
+	}
+	
+	@Test
+	public void testInvalid_rule2_2() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("LLI"));
+	}
+	
+	@Test
+	public void testInvalid_rule2_3() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("XDDI"));
+	}
+	
+	@Test
+	public void testInvalid_rule3_1() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("IL"));
+	}
+	
+	@Test
+	public void testInvalid_rule3_2() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("XD"));
+	}
+	
+	@Test
+	public void testInvalid_rule4_1() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("XXC"));
+	}
+	
+	@Test
+	public void testInvalid_rule4_2() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("IIV"));
+	}
+	
+	@Test
+	public void testInvalid_rule4_3() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("CCM"));
+	}
+	
+	@Test
+	public void testInvalid_rule5_1() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("VC"));
+	}
+	
+	@Test
+	public void testInvalid_rule5_2() {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("LC"));
+	}
+	
+	@Test
+	public void testInvalid_rule5_3() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to detect invalid input", false, RNum.checkValidity("DM"));
+	}
+	
+	@Test
+	public void testValid_1() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to accept valid input", true , RNum.checkValidity("LXXX"));
+	}
+	
+	@Test
+	public void testValid_2() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to accept valid input", true , RNum.checkValidity("CM"));
+	}
+	
+	@Test
+	public void testValid_3() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to accept valid input", true , RNum.checkValidity("MCMLXXXIV"));
+	}
+	
+	@Test
+	public void testValid_4() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to accept valid input", true , RNum.checkValidity("MMXIV"));
+	}
+	
+	@Test
+	public void testValid_5() {
+		RomanNumerals RNum = new RomanNumerals();	
+		assertEquals("Failed to accept valid input", true , RNum.checkValidity("MMM"));
+	}
+	
+	@Test (expected = InvalidInputException.class)
+	public void testConversion_InvalidInput_1() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();	
+		RNum.convertToInteger("XDDI");
+	}
+	
+	@Test (expected = InvalidInputException.class)
+	public void testConversion_InvalidInput_2() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		RNum.convertToInteger("XXC");
+	}
+	
+	@Test (expected = InvalidInputException.class)
+	public void testConversion_InvalidInput_3() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		RNum.convertToInteger("VC");
+	}
+	
+	@Test
+	public void testConversion_ValidInput_1() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 6, RNum.convertToInteger("VI"));
+	}
+	
+	@Test
+	public void testConversion_ValidInput_2() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 14, RNum.convertToInteger("XIV"));
+	}
+	
+	@Test
+	public void testConversion_ValidInput_3() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 79, RNum.convertToInteger("LXXIX"));
+	}
+	
+	@Test
+	public void testConversion_ValidInput_4() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 999, RNum.convertToInteger("CMXCIX"));
+	}
+	
+	@Test
+	public void testConversion_ValidInput_5() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 2017, RNum.convertToInteger("MMXVII"));
+	}
+	
+	@Test
+	public void testConversion_ValidInput_6() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 3999, RNum.convertToInteger("MMMCMXCIX"));
+	}
+	@Test
+	public void testConversion_ValidInput_7() throws InvalidInputException {
+		RomanNumerals RNum = new RomanNumerals();
+		assertEquals("Failed to convert correctly", 2647, RNum.convertToInteger("MMDCXLVII"));
+	}
 }
